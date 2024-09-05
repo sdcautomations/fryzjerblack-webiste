2db5009402f7fe8b409497123f03a17ab02f2f6a
sdcautomations <sdcautomations@gmail.com>
2024-09-05 10:28:36 +0200

    feat: add className prop to section-heading component

--- src/lib/home/components/section-header/section-header.tsx
+++ src/lib/home/components/section-header/section-header.tsx
@@ -1,11 +1,18 @@
+import classNames from "classnames";
 import styles from "./section-header.module.scss";
 
 import { SectionHeaderProps } from "./section-header.types";
 import { Text } from "@/home/components";
 
-export const SectionHeader = ({ heading, desc }: SectionHeaderProps) => {
+export const SectionHeader = ({
+  heading,
+  desc,
+  className,
+}: SectionHeaderProps) => {
+  const headingClassNames = classNames(styles["section-header"], className);
+
   return (
-    <div className={styles["section-header"]}>
+    <div className={headingClassNames}>
       <Text size="20px" weight="400" opacity={"0.7"}>
         {desc}
       </Text>
